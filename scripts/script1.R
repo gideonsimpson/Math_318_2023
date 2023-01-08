2ed8ed561c058931d0bcdea1b27ca84233b1551b
Gideon Simpson <gideon.simpson@gmail.com>
2023-01-08 18:00:14 -0500

    example script

---
+++ scripts/script1.R
@@ -0,0 +1,10 @@
+# reset the seed, 100 is arbitrary
+set.seed(100)
+# generate 10 random variables from U(5., 25.5)
+xvalues <- runif(10, min=5., max= 25.5)
+# print the variables
+print(xvalues)
+# square the variables and store in a new array
+x2alues <- xvalues^2
+# compute the mean and print it to the screen
+print(mean(x2values))
